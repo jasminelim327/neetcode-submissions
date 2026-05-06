a28149bf6aca5470ba0283bddfb3e402fdf27206
Jasmine Lim <77251890+jasminelim327@users.noreply.github.com>
2026-05-06 15:35:34 +0800

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+       let numSet = new Set()
+
+       for (let i = 0; i < nums.length; i ++){
+        numSet.add(nums[i])
+       }
+
+       return numSet.size != nums.length
+    }
+}
+
+
+
+// empty object 
+        // var check =  {"1": true ; "2" : true }
+        // var numDict = {}
+
+        // for (var i = 0 ; i < nums.length+1 ; i++){
+        //     console.log(numDict.nums[i])
+        //     if (numDict[nums[i]] == true ){ 
+        //         return true 
+        //     }
+        //     else{
+        //         numDict[nums[i]] = true
+        //     }
+        // }
+        // return false
+
+        // var set = new Set(nums).size
+        // return set != nums.length
+        
+
+        //  loop through the entire array 
+        //  return true if the integer is found as a key in the dictionary 
+        // if not it will add the integer to the object as key 
+
+        //  at the end of the array, means that duplicates is not found, then return false
