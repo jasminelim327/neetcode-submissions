05ea8967940de3d28c53ecaa199d8c91f0b40b47
Jasmine Lim <77251890+jasminelim327@users.noreply.github.com>
2026-05-14 10:42:44 +0800

    Add: replace-elements-with-greatest-element-on-right-side - submission-1

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+        let res = []
+
+        //  from the back to the front check if the current element is th
+        let largestNum = -1 // the lastNum 
+        for (let i = arr.length-1 ; i >= 0; i --){
+            let currNum = arr[i]
+            res[i] = largestNum
+            largestNum = Math.max(currNum, largestNum)
+        }
+        return res
+
+
+
+        
+    }
+}
