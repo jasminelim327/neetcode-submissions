20740c568286d7e63d456f50c2318f33d768577e
Jasmine Lim <77251890+jasminelim327@users.noreply.github.com>
2026-05-15 22:47:08 +0800

    Add: can-place-flowers - submission-9

---
+++ Data Structures & Algorithms/can-place-flowers/submission-9.js
@@ -0,0 +1,51 @@
+class Solution {
+    /**
+     * @param {number[]} flowerbed
+     * @param {number} n
+     * @return {boolean}
+     */
+    canPlaceFlowers(flowerbed, n) {
+
+        for (let i = 0; i < flowerbed.length; i++){
+            // edge case for last one and first one
+            console.log(n, "at the beginning of the loop")
+            if (n == 0){
+                return true
+            }
+
+            if (i == 0){
+                if (flowerbed[i] == 0 && flowerbed[i+1] != 1){
+                    console.log('deducted here')
+                    n-=1
+                }
+            }
+
+            else if (i == flowerbed.length-1){
+                if (flowerbed[i]== 0 && flowerbed[i-1] != 1){
+                    console.log('deducted here1')
+                    n-=1
+                    i++
+
+                }
+            }
+
+            else if  (flowerbed[i] == 1){
+                continue
+            }
+            
+            else if (flowerbed[i-1] == 1 || flowerbed[i+1] == 1){
+                continue
+            }
+
+            else if (flowerbed[i-1] == 0 || flowerbed[i+1] == 0) {
+                    console.log('deducted here3')
+                    n-=1
+                    i++
+            }
+        }
+        
+        console.log(n)
+        return n==0
+
+    }
+}
