81a131b26b464bbeb8dc0bf70a37a02936f7b80d
Jasmine Lim <77251890+jasminelim327@users.noreply.github.com>
2026-05-21 14:11:36 +0800

    Add: maximum-ascending-subarray-sum - submission-5

---
+++ Data Structures & Algorithms/maximum-ascending-subarray-sum/submission-5.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxAscendingSum(nums) {
+        let i = 0
+        let sum = nums[0]
+        let maxAscendingSum = 0
+
+        while (i < nums.length){
+            if (nums[i] > nums[i-1]){ // increasing 
+                sum += nums[i]
+            }
+            else {
+                sum = nums[i]
+            }
+            maxAscendingSum = Math.max(maxAscendingSum, sum)
+            i++
+        }
+
+        return Math.max(maxAscendingSum, sum)
+
+    }
+}
